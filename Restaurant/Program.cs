5220500da5cd358daa7abed731d04112b73cc3d4
Jonas Zdanys <jonas.zdanys@teliacompany.com>
2021-01-29 14:58:17 +0200

    complete

---
+++ Restaurant/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Restaurant
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Pizza> OrderQueue = GetOrders();
+            foreach (var item in OrderQueue)
+            {
+                item.Prepare();
+            }
+            List<Soup> warmup = Appitizers();
+            foreach (var soup in warmup)
+            {
+                soup.HeatThePot();
+            }
+        }
+
+        private static List<Pizza> GetOrders()
+        {
+            var orders = new List<Pizza>()
+            {
+                new Pizza("Margherita", new string[] { "tomatoes", "basil", "mozzarella" }),
+                new Pizza("Pepperoni", new string[] { "pepperoni", "mozzarella" }),
+            };
+            return orders;
+        }
+        private static List<Soup> Appitizers ()
+        {
+            var soups = new List<Soup>()
+            {
+                new Soup("My Favourite", new string[] { "chicken", "onion", "potatoes", "carrots", "egg-plant","paprika", "coriander", "celery"}, new string [] {"cumin", "salt", "pepper"})
+            };
+            return soups;
+        }
+    }
+}
---
+++ Restaurant/Soup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Restaurant
+{
+    public class Soup
+    {
+        private readonly string[] _ingredients;
+        private readonly string[] _spices;
+        public string Name { get; }
+
+        public Soup(string name, string[] ingredients, string [] seasoning)
+        {
+            Name = name;
+            _ingredients = ingredients;
+            _spices = seasoning;
+        }
+        public void HeatThePot()
+        {
+            Console.WriteLine($"Making {Name} soup.");
+            BoilingWater();
+            ChoppingIngredients();
+            AddingIngredients();
+            AddSeasoning();
+            WaitUntilItIsDone();
+            Console.WriteLine("Enjoy!!\n");
+        }
+        private void BoilingWater()
+        {
+            Console.WriteLine($"Boiling Water..");
+            Thread.Sleep(500);
+            Console.WriteLine($"Caution! HOT!!!");
+            Thread.Sleep(1000);
+        }
+        private void ChoppingIngredients()
+        {
+            foreach (var ingredient in _ingredients)
+            {
+                Console.WriteLine($"Chopping {ingredient}..");
+                Thread.Sleep(500);
+            }
+        }
+        private void AddingIngredients()
+        {
+            foreach (var ingredient in _ingredients)
+            {
+                Console.WriteLine($"Adding {ingredient} to the soup..");
+                Thread.Sleep(500);
+            }
+        }
+        private void AddSeasoning()
+        {
+            foreach (string spice in _spices)
+            {
+                Console.WriteLine($"Add a dash of {spice}..");
+                Thread.Sleep(500);
+            }
+        }
+        private void WaitUntilItIsDone()
+        {
+            Console.WriteLine($"Let it simmer..");
+            Thread.Sleep(1000);
+        }
+    }
+}
